31586165c09ab18b7e0138d46f3f6e3024e14682
Kendal Percimoney <kendal.percimoney@gmail.com>
2019-08-03 16:54:19 +0200

    Initial commit

---
+++ app/src/main/java/com/kendalpercimoney/complimentme/MainActivity.java
@@ -0,0 +1,69 @@
+package com.kendalpercimoney.complimentme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.VibrationEffect;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.os.Vibrator;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+
+    private ImageView ballIV;
+    private TextView answerTV;
+
+    //strings to push on click
+    private String[] answersArray = {"your perspective is refreshing. :)", "you’re more helpful than you realize. :)", " you’re even more beautiful on the inside than you are on the outside. :)",
+            "you're most incredible when you're not afraid to be yourself :)", "you're a candle in the darkness :)", "you're really something special :)",
+            "you're more fun than a ball pit filled with candy :)"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+        ballIV = findViewById(R.id.btn);
+        answerTV = findViewById(R.id.answer);
+
+        ballIV.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn:
+
+                answerTV.setText("hold on....");
+
+                int rand = new Random().nextInt(answersArray.length);
+
+                answerTV.setText(answersArray[rand]);
+
+                //vibrate on touch
+                Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    v.vibrate(VibrationEffect.createOneShot(50, VibrationEffect.DEFAULT_AMPLITUDE));
+                } else {
+                    //deprecated in API 26
+                    v.vibrate(50);
+                }
+                //v.vibrate(VibrationEffect.createOneShot(500, VibrationEffect.DEFAULT_AMPLITUDE));
+
+                break;
+
+        }
+
+    }
+}
